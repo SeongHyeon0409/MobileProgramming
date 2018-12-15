c5eba62762e1588d2224825ad1764d79b79fc715
SEONGHYEON\t1397 <t139754@kookmin.ac.kr>
2018-12-15 19:40:00 +0900

    Register Layout

---
+++ app/src/main/java/com/kmu/timemanager/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.kmu.timemanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView backButton = (TextView) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent backIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                RegisterActivity.this.startActivity(backIntent);
+            }
+        });
+    }
+}
