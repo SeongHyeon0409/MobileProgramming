ed7c6740dc4c7456f693e5425a86199baca4f64b
SEONGHYEON\t1397 <t139754@kookmin.ac.kr>
2018-12-16 22:36:15 +0900

    강의목록 spinner추가

--- app/src/main/java/com/kmu/timemanager/CourseFragment.java
+++ app/src/main/java/com/kmu/timemanager/CourseFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 
 /**
@@ -60,6 +64,53 @@ public class CourseFragment extends Fragment {
         }
     }
 
+    private ArrayAdapter yearAdapter;
+    private Spinner yearSpinner;
+    private ArrayAdapter termAdapter;
+    private Spinner termSpinner;
+    private ArrayAdapter areaAdapter;
+    private Spinner areaSpinner;
+
+    private String courseUniversity = "";
+    private String courseYear = "";
+    private String courseTerm = "";
+    private String courseArea = "";
+
+    public void onActivityCreated(Bundle b) {
+        super.onActivityCreated(b);
+
+        final RadioGroup courseUniversityGroup  = (RadioGroup) getView().findViewById(R.id.courseUniversityGroup);
+        yearSpinner = (Spinner) getView().findViewById(R.id.yearSpinner);
+        termSpinner = (Spinner) getView().findViewById(R.id.termSpinner);
+        areaSpinner = (Spinner) getView().findViewById(R.id.areaSpinner);
+
+        courseUniversityGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton courseButton = (RadioButton) getView().findViewById(checkedId);
+                courseUniversity = courseButton.getText().toString();
+
+                yearAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.year, android.R.layout.simple_spinner_dropdown_item);
+                yearSpinner.setAdapter(yearAdapter);
+
+                termAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.term, android.R.layout.simple_spinner_dropdown_item);
+                termSpinner.setAdapter(termAdapter);
+
+                if(courseUniversity.equals("학부")) {
+                    areaAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.universityArea, android.R.layout.simple_spinner_dropdown_item);
+                    areaSpinner.setAdapter(areaAdapter);
+                }
+
+                else if(courseUniversity.equals("대학원")) {
+                    areaAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.graduateArea, android.R.layout.simple_spinner_dropdown_item);
+                    areaSpinner.setAdapter(areaAdapter);
+                }
+
+            }
+        });
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -73,7 +124,6 @@ public class CourseFragment extends Fragment {
             mListener.onFragmentInteraction(uri);
         }
     }
-
 
     @Override
     public void onDetach() {
